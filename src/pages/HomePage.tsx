edd6ff8190bab0c4c4cb3cb0f77b05c0a2de7fde
anashwarapp <anashwararajanpp@gmail.com>
2025-01-26 13:38:28 +0530

    Removed boilerplate code and added theme

---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@mui/material";
+
+const HomePage = () => {
+  return (
+    <div>
+      <Button variant="contained">Hi</Button>
+    </div>
+  )
+}
+
+export default HomePage
